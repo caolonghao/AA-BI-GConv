5e0b462c32b52f18c5451069f747a1db4b690c65
TonyCao <tonycao0918@qq.com>
2023-02-07 19:59:29 +0800

    fix args error in test.py

--- test_odoc.py
+++ test_odoc.py
@@ -53,7 +53,7 @@ saved_model_path = os.path.join(snapshot_path, "best_model.pth")
 
 
 if __name__ == "__main__":
-    model = ODOC_seg_edge(seg_type=args.seg_type)
+    model = ODOC_seg_edge(postgnn=args.postgnn)
     model = model.cuda()
 
     db_test = ODOC(base_dir=train_data_path, split="test")
